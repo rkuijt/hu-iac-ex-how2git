1300d171e6c579352d96e5fe256c6918d830eeee
Dahir Warsame <dahirwarsame@gmail.com>
2021-04-12 17:14:20 +0200

    removed password

--- src/main/java/nl/rkuijt/domain/Goose.java
+++ src/main/java/nl/rkuijt/domain/Goose.java
@@ -5,7 +5,7 @@ public class Goose {
 	private String name;
 	private String sound;
 	private int size;
-	private String password = "***REMOVED***";
+	private String password = "";
 
 	public Goose() {
 	}
